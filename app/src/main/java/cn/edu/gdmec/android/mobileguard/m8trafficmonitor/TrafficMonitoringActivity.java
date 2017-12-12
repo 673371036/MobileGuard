26b8b92a13d74a69cee073bff37f4f309d147626
1746814899@qq.com <1746814899@qq.com>
2017-12-12 15:40:40 +0800

    8.5.

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/TrafficMonitoringActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/TrafficMonitoringActivity.java
@@ -3,17 +3,19 @@ package cn.edu.gdmec.android.mobileguard.m8trafficmonitor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by student on 17/12/11.
  */
 
 public class TrafficMonitoringActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button mCorrectFlowBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        
 
     }
 
