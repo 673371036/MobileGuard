9060817dba03d977e55d7d70d0ba17bd0c5f9466
1746814899@qq.com <1746814899@qq.com>
2017-12-19 15:19:03 +0800

    8

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/reciever/BootCompleteReciever.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/reciever/BootCompleteReciever.java
@@ -3,6 +3,9 @@ package cn.edu.gdmec.android.mobileguard.m8trafficmonitor.reciever;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitoringService;
 
 /**
  * Created by student on 17/12/11.
@@ -11,6 +14,10 @@ import android.content.Intent;
 public class BootCompleteReciever extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
+//        if (!SystemInfoUtils.isServiceRunning(context,"cn.edu.gdmec.android.mobileguard.mBtrafficmonitor.service.TrafficMonitoringService")) {
+//            Log.d("traffic service","turn on");
+//            context.startService(new Intent(context, TrafficMonitoringService.class));
+//        }
 
 
     }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/utils/SystemlnfoUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/utils/SystemlnfoUtils.java
@@ -11,6 +11,18 @@ import java.util.List;
 
 public class SystemlnfoUtils {
 
+    public static boolean isServiceRunning(Context context,String className) {
+        ActivityManager am=(ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> infos=am.getRunningServices(200);
+        for (ActivityManager.RunningServiceInfo info:infos) {
+            String serviceClassName = info.service.getClassName();
+            if (className.equals(serviceClassName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 
 
